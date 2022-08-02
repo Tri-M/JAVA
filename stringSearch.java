f40dc4717b1feb35d2f5978009256243ad4010b1
Trisha M <trisham0910@gmail.com>
2022-08-02 11:11:21 +0530

    Create stringSearch.java

---
+++ stringSearch.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Main {
+
+	public static void main(String[] args)
+	{
+		String str= "Hello World Hello";
+		int firstIndex = str.indexOf("World");
+
+		System.out.println("First occurrence of string"+ " is found at : "+ firstIndex);
+
+	}
+}
