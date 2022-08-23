09544a86b12b80e82c98b7a0120ae07c5ef4b067
Trisha M <trisham0910@gmail.com>
2022-08-23 10:45:34 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Player {
+    String name;
+    int age;
+    String Country;
+    public String getName(){
+        return name;
+    }
+    public void setName(String newName) {
+        this.name = newName;
+    }
+    Player(){}
+    Player(String name,int age, String Country){
+
+    }
+    public String toString(){
+        return "Name:"+this.name+" Age:"+this.age;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
